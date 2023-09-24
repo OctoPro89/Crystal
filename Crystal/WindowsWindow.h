f8fa8297cfaded4ac919b09f0a0bfb6436037025
OctoPro89 <robertvs8910@gmail.com>
2023-09-23 21:49:28 -0400

    update

---
+++ Crystal/WindowsWindow.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Crystal/Window.h"
+
+#include <GLFW/glfw3.h>
+
+namespace Crystal {
+	class WindowsWindow : public Window {
+	public:
+		WindowsWindow(const WindowProps& props);
+		virtual ~WindowsWindow();
+
+		void OnUpdate() override;
+
+		inline unsigned int GetWidth() const override { return m_Data.Width; }
+		inline unsigned int GetHeight() const override { return m_Data.Height; }
+	};
+}
