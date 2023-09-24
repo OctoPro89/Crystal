40a1863d1373f92e4dcb5bcc0c7bf06dd3e10ee7
OctoPro89 <robertvs8910@gmail.com>
2023-09-23 22:13:31 -0400

    update files

--- Crystal/src/Crystal/Core.h
+++ Crystal/src/Crystal/Core.h
@@ -10,4 +10,12 @@
 	#error OS not supported by Crystal!
 #endif
 
+#ifdef CRYSTAL_ENABLE_ASSERTS
+	#define CRYSTAL_ASSERT(x, ...) { if(!(x)) { CRYSTAL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define CRYSTAL_CORE_ASSERT(x, ...) { if(!(x)) { CRYSTAL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define CRYSTAL_ASSERT(x, ...)
+	#define CRYSTAL_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
