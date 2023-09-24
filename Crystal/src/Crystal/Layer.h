c29fd0b3a58fcb45f6b760cbdca9ec2565393e44
OctoPro89 <robertvs8910@gmail.com>
2023-09-24 15:04:11 -0400

    Update

---
+++ Crystal/src/Crystal/Layer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Crystal/Core.h"
+#include "Crystal/Events/Event.h"
+
+namespace Crystal {
+	class CRYSTAL_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+}
