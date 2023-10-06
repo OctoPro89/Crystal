650dc2a2447d8c81afc430139171e09b1f565415
OctoPro89 <robertvs8910@gmail.com>
2023-10-05 21:36:27 -0400

    Starting 2D Renderer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -180,6 +180,9 @@ public:
 
 		Crystal::Renderer::BeginScene(m_Camera);
 
+		Crystal::Renderer2D::BeginScene(m_Camera);
+		Crystal::Renderer2D::DrawQuad();
+
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
 		std::dynamic_pointer_cast<Crystal::OpenGLShader>(m_Shader)->Bind();
