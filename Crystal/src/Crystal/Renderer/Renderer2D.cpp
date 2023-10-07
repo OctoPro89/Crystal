4fe4cc076bfb850552922afdaefb2f71b73a5b75
OctoPro89 <robertvs8910@gmail.com>
2023-10-07 10:27:39 -0400

    Start 2D Renderer work

---
+++ Crystal/src/Crystal/Renderer/Renderer2D.cpp
@@ -0,0 +1,26 @@
+#include "Renderer2D.h"
+
+namespace Crystal
+{
+
+	void Renderer2D::Init()
+	{
+
+	}
+
+	void Renderer2D::Shutdown()
+	{
+
+	}
+
+	void Renderer2D::BeginScene(OrthographicCamera& camera)
+	{
+
+	}
+
+	void Renderer2D::EndScene()
+	{
+
+	}
+
+}
---
+++ Crystal/src/Crystal/Renderer/Renderer2D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "OrthographicCamera.h"
+
+namespace Crystal
+{
+	class Renderer2D
+	{
+	public:
+		static void Init();
+		static void Shutdown();
+
+		static void BeginScene(OrthographicCamera& camera);
+		static void EndScene();
+
+		// Primitives
+	};
+}
